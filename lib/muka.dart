50af1ff669397980207b33a65d84ef2980f31362
Shackles Butterfly <Spicely@outlook.com>
2020-06-15 14:15:11 +0800

    []

--- lib/muka.dart
+++ lib/muka.dart
@@ -6,6 +6,7 @@ import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:flutter/services.dart';
+import 'package:muka/src/http_res.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:install_plugin/install_plugin.dart';
@@ -13,7 +14,6 @@ import 'package:package_info/package_info.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'model/update/index.dart';
-import 'model/Response/index.dart';
 
 part 'src/http_utils.dart';
 part 'src/dialog_utils.dart';
