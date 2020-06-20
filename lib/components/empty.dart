4cff7e0f5956eb2dc3594c5d856959c9b43b9f73
Spicely <Spicely@outlook.com>
2020-06-20 15:23:06 +0800

    更新Empty使用

--- lib/components/empty.dart
+++ lib/components/empty.dart
@@ -7,9 +7,8 @@ part of muka;
 //// Date: 2020年06月19日 20:33:24 Friday
 //////////////////////////////////////////////////////////////////////////
 class Empty extends StatefulWidget {
-  Empty({@required this.initLoad, this.child, this.empty, this.network})
-      : assert(initLoad != null, '初始请求必须填写'),
-        assert(GLOBAL_EMPTY_DATA_URL.isNotEmpty || empty != null, '你需要至少保证全局空数据图片地址或无数据填充其中一个有值'),
+  Empty({this.controller, this.child, this.empty, this.network})
+      : assert(GLOBAL_EMPTY_DATA_URL.isNotEmpty || empty != null, '你需要至少保证全局空数据图片地址或无数据填充其中一个有值'),
         assert(GLOBAL_NOT_NETWORK_URL.isNotEmpty || network != null, '你需要至少保证全局无网络图片地址或无网络填充其中一个有值');
 
   /// 空数据图片地址
@@ -25,12 +24,8 @@ class Empty extends StatefulWidget {
   /// 全局图片宽度
   static double IMG_WIDTH = 120.0;
 
-  /// 初始请求
-  ///
-  /// 需要返回状态
-  ///
-  /// true 成功不显示 false 失败显示图片
-  final Future<bool> Function() initLoad;
+  /// 控制器
+  final EmptyController controller;
 
   final Widget child;
 
@@ -46,8 +41,8 @@ class Empty extends StatefulWidget {
 
 class _EmptyState extends State<Empty> {
   /// 错误状态
-  /// true不显示 false 显示
-  bool _status = true;
+  /// true显示 false 不显示
+  bool _status = false;
 
   /// 网络状态
   ///
@@ -57,14 +52,21 @@ class _EmptyState extends State<Empty> {
   @override
   initState() {
     super.initState();
-    Future.delayed(Duration(microseconds: 500), () async {
-      _status = await widget.initLoad();
-      ConnectivityResult connectivityResult = await Connectivity().checkConnectivity();
-      if (connectivityResult == ConnectivityResult.none) {
-        _network = false;
-      }
-      setState(() {});
-    });
+    if (widget.controller == null) {
+      Future.delayed(Duration(microseconds: 500), () async {
+        _network = await _getNetwork();
+        if(widget.child == null) {
+          _status = true;
+        }
+        setState(() {});
+      });
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    widget.controller?._bindEmptyState(this);
   }
 
   @override
@@ -73,7 +75,7 @@ class _EmptyState extends State<Empty> {
       children: <Widget>[
         widget.child ?? Container(),
         Offstage(
-          offstage: _status,
+          offstage: !_status,
           child: Container(
             width: double.infinity,
             height: double.infinity,
@@ -87,8 +89,52 @@ class _EmptyState extends State<Empty> {
 
   Widget _getImage() {
     if (_network) {
-      return widget.empty ?? Image.asset(Empty.GLOBAL_EMPTY_DATA_URL, width: Empty.IMG_WIDTH,);
+      return widget.empty ??
+          Image.asset(
+            Empty.GLOBAL_EMPTY_DATA_URL,
+            width: Empty.IMG_WIDTH,
+          );
     }
     return widget.network ?? Image.asset(Empty.GLOBAL_NOT_NETWORK_URL, width: Empty.IMG_WIDTH);
+  }
+
+  Future<bool> _getNetwork() async {
+    ConnectivityResult connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      return false;
+    }
+    return true;
+  }
+
+  void _setVisible(bool status) async {
+    _status = status;
+    if (status) {
+      _network = await _getNetwork();
+    }
+    setState(() {});
+  }
+}
+
+class EmptyController {
+  _EmptyState _easyState;
+
+  /// 绑定状态
+  void _bindEmptyState(_EmptyState state) {
+    this._easyState = state;
+  }
+
+  /// 显示
+  void show() {
+    _easyState._setVisible(true);
+  }
+
+  /// 隐藏
+  void hide() {
+    _easyState._setVisible(false);
+  }
+
+  /// 销毁
+  void dispose() {
+    this._easyState = null;
   }
 }
