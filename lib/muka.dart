d4e1386135147605f1bf2b9583c7713699ffc888
Spicely <Spicely@outlook.com>
2021-03-02 18:25:17 +0800

    移除web cookie设置 会引起其他平台报错

--- lib/muka.dart
+++ lib/muka.dart
@@ -5,7 +5,6 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:connectivity/connectivity.dart';
 import 'package:dio/adapter.dart';
-import 'package:dio/adapter_browser.dart';
 // import 'package:dio/adapter_browser.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
