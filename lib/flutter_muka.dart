9c1d720de99d0283bc097032452d272fbac7f0f2
Spicely <Spicely@outlook.com>
2021-07-17 18:13:58 +0800

    修复打包app失败问题

--- lib/flutter_muka.dart
+++ lib/flutter_muka.dart
@@ -15,7 +15,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:r_upgrade/r_upgrade.dart';
 import 'package:scan/scan.dart';
-import 'package:dio/adapter_browser.dart';
+import 'src/adapter_browser.dart' if (dart.library.js) 'package:dio/adapter_browser.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
