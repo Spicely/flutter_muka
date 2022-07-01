51a4a41273eabf7927bfef5a9764d2ad7f0fe493
Spicely <Spicely@outlook.com>
2022-07-01 14:12:21 +0800

    []

--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,18 +3,13 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  connectivity_plus_windows
   permission_handler_windows
-<<<<<<< HEAD
 )
 
-<<<<<<< HEAD
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-=======
->>>>>>> 20fbb3c9cc2685119294aca580f8efc45d1298b9
 )
 
-=======
->>>>>>> dev2
 set(PLUGIN_BUNDLED_LIBRARIES)
 
 foreach(plugin ${FLUTTER_PLUGIN_LIST})
@@ -23,3 +18,8 @@ foreach(plugin ${FLUTTER_PLUGIN_LIST})
   list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
 endforeach(plugin)
+
+foreach(ffi_plugin ${FLUTTER_FFI_PLUGIN_LIST})
+  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin}/windows plugins/${ffi_plugin})
+  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${ffi_plugin}_bundled_libraries})
+endforeach(ffi_plugin)
