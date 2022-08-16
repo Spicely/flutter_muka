9875d66f5dd64eedfd92a032c417aeda53ef0d46
Spicely <Spicely@outlook.com>
2022-08-17 01:55:49 +0800

    修改FututeLayoutBuilder

--- lib/components/future_layout_builder.dart
+++ lib/components/future_layout_builder.dart
@@ -3,7 +3,7 @@
  * Created Date: 2022-08-07 22:37:06
  * Author: Spicely
  * -----
- * Last Modified: 2022-08-15 17:23:39
+ * Last Modified: 2022-08-17 00:51:24
  * Modified By: Spicely
  * -----
  * Copyright (c) 2022 Spicely Inc.
@@ -18,46 +18,109 @@ part of flutter_muka;
 
 MukaFutureLayoutBuilderTheme _futureLayoutBuilderTheme = MukaConfig.config.futureLayoutBuilderTheme;
 
-class FutureLayoutBuilder<T> extends StatelessWidget {
+// class FutureLayoutBuilder<T> extends StatelessWidget {
+//   final Widget Function(T) builder;
+
+//   final Function()? future;
+
+//   final MukaFutureLayoutBuilderTheme? config;
+
+//   late Future<T>? _future;
+
+//   FutureLayoutBuilder({
+//     Key? key,
+//     required this.builder,
+//     this.future,
+//     this.config,
+//   }) : super(key: key) {
+//     _future = onFuture();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder<T>(
+//       future: _future,
+//       builder: (context, snapshot) {
+//         if (snapshot.hasError) {
+//           return config?.errorWidget(snapshot.data) ?? _futureLayoutBuilderTheme.errorWidget(snapshot.data);
+//         } else if (snapshot.hasData) {
+//           if (snapshot.data is Exception || snapshot.data is DioError) {
+//             return config?.errorWidget(snapshot.data) ?? _futureLayoutBuilderTheme.errorWidget(snapshot.data);
+//           } else {
+//             return builder(snapshot.data!);
+//           }
+//         } else {
+//           return config?.loadingWidget ?? _futureLayoutBuilderTheme.loadingWidget;
+//         }
+//       },
+//     );
+//   }
+
+//   Future<T> onFuture() async {
+//     dynamic res = await future?.call();
+//     return res ?? true;
+//   }
+// }
+
+class FutureLayoutBuilder<T> extends StatefulWidget {
   final Widget Function(T) builder;
 
   final Function()? future;
 
   final MukaFutureLayoutBuilderTheme? config;
 
-  late Future<T>? _future;
-
-  FutureLayoutBuilder({
+  const FutureLayoutBuilder({
     Key? key,
     required this.builder,
     this.future,
     this.config,
-  }) : super(key: key) {
+  }) : super(key: key);
+  @override
+  _FutureLayoutBuilderState<T> createState() => _FutureLayoutBuilderState<T>();
+}
+
+class _FutureLayoutBuilderState<T> extends State<FutureLayoutBuilder<T>> {
+  late Future<dynamic> _future;
+  @override
+  initState() {
+    super.initState();
     _future = onFuture();
   }
 
   @override
+  void didUpdateWidget(covariant FutureLayoutBuilder<T> oldWidget) {
+    if (widget.future != oldWidget.future) {
+      _future = onFuture();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<T>(
+    return FutureBuilder(
       future: _future,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return config?.errorWidget(snapshot.data) ?? _futureLayoutBuilderTheme.errorWidget(snapshot.data);
-        } else if (snapshot.hasData) {
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
           if (snapshot.data is Exception || snapshot.data is DioError) {
-            return config?.errorWidget(snapshot.data) ?? _futureLayoutBuilderTheme.errorWidget(snapshot.data);
+            return widget.config?.errorWidget(snapshot.data) ?? _futureLayoutBuilderTheme.errorWidget(snapshot.data);
           } else {
-            return builder(snapshot.data!);
+            return widget.builder(snapshot.data);
           }
+        } else if (snapshot.hasError) {
+          return widget.config?.errorWidget(snapshot.data) ?? _futureLayoutBuilderTheme.errorWidget(snapshot.data);
         } else {
-          return config?.loadingWidget ?? _futureLayoutBuilderTheme.loadingWidget;
+          return widget.config?.loadingWidget ?? _futureLayoutBuilderTheme.loadingWidget;
         }
       },
     );
   }
 
-  Future<T> onFuture() async {
-    dynamic res = await future?.call();
-    return res ?? true;
+  Future<dynamic> onFuture() async {
+    try {
+      dynamic res = await widget.future?.call();
+      return res ?? true;
+    } catch (e) {
+      return e;
+    }
   }
 }
